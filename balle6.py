338fdbcc0af0b00e1dc76cf16a75dddbecf24b1b
Gu1zGu1z <117632588+Gu1zGu1z@users.noreply.github.com>
2022-11-21 09:41:53 +0100

    Update balle6.py

--- balle6.py
+++ balle6.py
@@ -1,30 +1,26 @@
-import pyxel, random
+import pyxel
 
 # taille de la fenetre 128x128 pixels
 # ne pas modifier
-pyxel.init(128, 128, title="Casse brique")
+pyxel.init(128, 128, title="Nuit du c0de")
 
-# position initiale du plateau
-# (origine des positions : milieu inférieur)
+# position initiale du vaisseau
+# (origine des positions : coin haut gauche)
 plateau_x = 64
 plateau_y = 115
-vies = 4
-
-# chargement des images
-pyxel.load("images.pyxres")
-
 
 def plateau_deplacement(x, y):
     """déplacement avec les touches de directions"""
 
     if pyxel.btn(pyxel.KEY_RIGHT):
         if (x < 120) :
-            x = x + 1
+            x = x + 2
     if pyxel.btn(pyxel.KEY_LEFT):
         if (x > 0) :
-            x = x - 1
+            x = x - 2
     return x, y
-               
+
+
 # =========================================================
 # == UPDATE
 # =========================================================
@@ -32,8 +28,10 @@ def update():
     """mise à jour des variables (30 fois par seconde)"""
 
     global plateau_x, plateau_y
+
     # mise à jour de la position du vaisseau
     plateau_x, plateau_y = plateau_deplacement(plateau_x, plateau_y)
+
 
 # =========================================================
 # == DRAW
@@ -44,18 +42,7 @@ def draw():
     # vide la fenetre
     pyxel.cls(0)
 
-    # si le vaisseau possede des vies le jeu continue
-    if vies > 0:
-
-        # affichage des vies            
-        pyxel.text(5,5, 'VIES:'+ str(vies), 7)
-
-        # plateau (carre 8x8)
-        pyxel.blt(plateau_x, plateau_y, 0, 0, 0, 8, 8)    
-
-    # sinon: GAME OVER
-    else:
-
-        pyxel.text(50,64, 'GAME OVER', 7)
+    # vaisseau (carre 8x8)
+    pyxel.rect(plateau_x, plateau_y, 8, 8, 1)
 
 pyxel.run(update, draw)
